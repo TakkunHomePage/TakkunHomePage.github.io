58c89e8dc42fd0db14b9b092c5d43f67ac2ca381
TakkunMC_jp <Kaneda.family.803@gmail.com>
2026-03-08 19:00:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+
+particlesJS("particles-js",{
+particles:{
+number:{value:80},
+size:{value:3},
+move:{speed:2},
+line_linked:{enable:true}
+}
+})
+
+
 function acceptCookies(){
 
 localStorage.setItem("cookieAccepted","true")
-
 document.getElementById("cookieBanner").style.display="none"
 
 }
@@ -12,8 +22,29 @@ document.getElementById("cookieBanner").style.display="none"
 
 }
 
+
 function closeAnnouncement(){
 
 document.getElementById("announcement").style.display="none"
 
 }
+
+
+const reveals=document.querySelectorAll(".reveal")
+
+window.addEventListener("scroll",()=>{
+
+reveals.forEach(r=>{
+
+const top=r.getBoundingClientRect().top
+const windowHeight=window.innerHeight
+
+if(top<windowHeight-100){
+
+r.classList.add("active")
+
+}
+
+})
+
+})
